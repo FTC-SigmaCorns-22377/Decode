d9960b36c5e6230af898cad57e162fbc85c10d7c
dolphinpod <thomas.fasan2@gmail.com>
2025-09-22 01:56:07 -0400

    refactor: make State use Double instead of Float

--- TeamCode/src/main/java/sigmacorns/State.kt
+++ TeamCode/src/main/java/sigmacorns/State.kt
@@ -1,10 +1,10 @@
 package sigmacorns
 
 data class State (
-    var flywheelSpeed: Float,
-    val driveTrainPosition: FloatArray,
-    val driveTrainVelocity: Float,
-    val driveTrainAcceleration: Float,
-    val intakeFlapPosition: Float,
-    val intakeRollerPower: Float
+    var flywheelSpeed: Double,
+    val driveTrainPosition: DoubleArray,
+    val driveTrainVelocity: Double,
+    val driveTrainAcceleration: Double,
+    val intakeFlapPosition: Double,
+    val intakeRollerPower: Double
 )
