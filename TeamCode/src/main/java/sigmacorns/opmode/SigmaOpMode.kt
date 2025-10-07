fc9e31ad275cf4feaf1ec3940171a1bed2e3a5be
dolphinpod <thomas.fasan2@gmail.com>
2025-10-06 20:43:26 -0400

    fix(opmode): SigmaOpMode auto inits HardwareIO and starts opmode while in sim

--- TeamCode/src/main/java/sigmacorns/opmode/SigmaOpMode.kt
+++ TeamCode/src/main/java/sigmacorns/opmode/SigmaOpMode.kt
@@ -1,21 +1,67 @@
 package sigmacorns.opmode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import sigmacorns.State
 import sigmacorns.constants.Network
+import sigmacorns.io.HardwareIO
 import sigmacorns.io.SigmaIO
+import sigmacorns.io.SimIO
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
-abstract class SigmaOpMode(val io: SigmaIO): LinearOpMode() {
-    var SIM: Boolean = false
+abstract class SigmaOpMode(
+    private val providedIO: SigmaIO? = null
+): LinearOpMode() {
+    val io: SigmaIO by lazy {
+        providedIO ?: HardwareIO(hardwareMap)
+    }
+
+    var SIM: Boolean = io is SimIO
     var LIMELIGHT_CONNECTED: Boolean = true
 
-    fun solverIP() = if(LIMELIGHT_CONNECTED) Network.LIMELIGHT else Network.SIM_MPC
-    fun rerunIP() = if(SIM) Network.SIM_RERUN else Network.ROBOT_RERUN
+    private val internalState by lazy { OpModeReflection(this) }
+
+    fun solverIP() = if (LIMELIGHT_CONNECTED) Network.LIMELIGHT else Network.SIM_MPC
+    fun rerunIP() = if (SIM) Network.SIM_RERUN else Network.ROBOT_RERUN
+
+    fun ioLoop(f: (State) -> Boolean) {
+        val state = State(io)
+        while (opModeIsActive()) {
+            state.update(io)
+            if(f(state)) return
+            io.update()
+        }
+    }
 
     override fun waitForStart() {
-        if(SIM) {
-            return
-        } else {
+        if (!SIM) {
             super.waitForStart()
+            return
         }
+
+        if (!internalState.isStarted()) {
+            internalState.markStarted()
+        }
+    }
+
+    // Reflection bridge to access FTC SDK private opmode state.
+    private class OpModeReflection(private val target: SigmaOpMode) {
+        private val opModeInternalClass = Class.forName("com.qualcomm.robotcore.eventloop.opmode.OpModeInternal")
+        private val linearOpModeClass = Class.forName("com.qualcomm.robotcore.eventloop.opmode.LinearOpMode")
+        private val isStartedField = opModeInternalClass.getDeclaredField("isStarted").apply { isAccessible = true }
+        private val userMonitoredForStartField = linearOpModeClass.getDeclaredField("userMonitoredForStart").apply { isAccessible = true }
+
+        fun isStarted(): Boolean = isStartedField.getBoolean(target)
+
+        fun markStarted() {
+            isStartedField.setBoolean(target, true)
+            userMonitoredForStartField.setBoolean(target, true)
+        }
+    }
+
+    companion object {
+        private val SIM_DEFAULT_INIT_DURATION: Duration = 1.seconds
+        private val SIM_WAIT_INTERVAL: Duration = 10.milliseconds
     }
 }
